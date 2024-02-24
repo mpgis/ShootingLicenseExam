a6b52c3de6461541f37b4d1fea866bd6f1d10cba
James <kuba.slusarski@gmail.com>
2024-02-24 09:31:15 +0100

    Initial commit

---
+++ ShootingLicenseExam/ShootingLicenseExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootingLicenseExamApp.swift
+//  ShootingLicenseExam
+//
+//  Created by Jakub Slusarski on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShootingLicenseExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
